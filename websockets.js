fd8bbe53ab6f99bf2aea9fe3c68e1a27d06bdf12
anuroopmallick <anuroopmallick1@gmail.com>
2025-07-24 12:37:31 +0530

    websockets using socket.io

---
+++ websockets.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+app.use(express.static("public"));
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user is connected", socket.id);
+  setInterval(() => {
+    socket.emit("message", "message from server-" + socket.id + new Date());
+  }, 5000);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+
+  socket.on("message", (data) => {
+    socket.broadcast.emit("broadcast", data);
+  });
+});
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
